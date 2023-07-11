4d8aae82622469a43b7b7381753a7c2c7e7362f7
debojyoti369 <debojyoti.m.in@rupeek.com>
2023-07-11 12:20:40 +0530

    added localstorage for showing the datas after refresh for a session

--- src/App.js
+++ src/App.js
@@ -1,67 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ToDoLists from "./ToDoLists";
 
-const App = () =>
-{
-    const [inputList, setInputList] = useState("");
-    const [Items, setItems] = useState([]);
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
 
-    const itemEvent=(event)=>{
-        setInputList(event.target.value);
-    };
+  // Load items from local storage when the component mounts
+  useEffect(() => {
+    const storedItems = localStorage.getItem("todoItems");
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
 
-    const listOfItems = () =>
-    {
-       setItems((oldItems) =>
-       {
-          return [...oldItems, inputList];
-       });
-       setInputList("");
-    };
+  // Update local storage when Items state changes
+  useEffect(() => {
+    localStorage.setItem("todoItems", JSON.stringify(Items));
+  }, [Items]);
 
-    const deleteItems = (id) =>{
-         console.log("deleted");  
-         
-         setItems((oldItems) => {
-            return oldItems.filter((arrElem, index)=> {
-                return index !== id;
-            });
-         });
-    };
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
 
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
 
+  const deleteItems = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
 
-    return(
-        <>
-            <div className="main_div">
-               <div className="center_div">
-                    <br />
-                    <h1 className="Cursor" onClick={() =>
-          (window.location =
-            'https://github.com/jyoti369/todo_list')
-        }>ToDo List</h1>
-                    <br />
-                    <input type="text" placeholder="Add an Item" 
-                        value={inputList}
-                        onChange={itemEvent}
-                    />
-                    <button onClick={listOfItems}> + </button>
-                    
-                     <ol>
-                         {/* <li>{inputList}</li> */}
-                         {Items.map((itemval, index) =>
-                         {
-                           return <ToDoLists key={index}
-                           id={index}
-                           text={itemval} 
-                           onSelect={deleteItems}    
-                           />;
-                         })}
-                     </ol>
-               </div>
-            </div>
-        </>
-);
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1
+            className="Cursor"
+            onClick={() =>
+              (window.location =
+                "https://github.com/debojyoti369/todo-list")
+            }
+          >
+            ToDo List
+          </h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add an Item"
+            value={inputList}
+            onChange={itemEvent}
+          />
+          <button onClick={listOfItems}> + </button>
+
+          <ol>
+            {Items.map((itemval, index) => {
+              return (
+                <ToDoLists
+                  key={index}
+                  id={index}
+                  text={itemval}
+                  onSelect={deleteItems}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
